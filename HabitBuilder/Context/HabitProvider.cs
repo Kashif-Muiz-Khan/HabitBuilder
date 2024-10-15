e6bf7a5b3f733751cfbb3e97fc88384c2f76f9cb
Kashif-Muiz-Khan <khankashifmuiz@gmail.com>
2024-10-15 09:26:59 +0100

    Added Habit and User Providers

---
+++ HabitBuilder/Context/HabitProvider.cs
@@ -0,0 +1,38 @@
+﻿using HabitBuilder.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace HabitBuilder.Context
+{
+    public class HabitProvider
+    {
+        private readonly DatabaseContext _context;
+
+        public HabitProvider(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Habit>> GetAllHabitsAsync()
+        {
+            return await _context.Habits.OrderBy(habits => habits.Name).ToListAsync();
+        }
+
+        public async Task AddHabitAsync(Habit habits)
+        {
+            _context.Habits.Add(habits);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteHabitAsync(Habit habits)
+        {
+            _context.Habits.Remove(habits);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateHabitAsync(Habit habits)
+        {
+            _context.Habits.Update(habits);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
